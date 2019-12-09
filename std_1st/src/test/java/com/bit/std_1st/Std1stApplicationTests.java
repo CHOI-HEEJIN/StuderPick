c2b4b426224d373bec0a134dacd6094898cf4416
CHOI-HEEJIN <jatket@naver.com>
2019-12-09 17:49:05 +0900

    adding files

---
+++ std_1st/src/test/java/com/bit/std_1st/Std1stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.std_1st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Std1stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
